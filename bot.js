73cb21be0fc931d07cbf69d966d978b25b8fddb5
Sellmox5 <42848880+SPLITLPTEOEORK2003@users.noreply.github.com>
2018-11-29 18:57:40 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,43 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "S";
+var adminprefix = 'S'
+const developers = ["350547695569600522"]
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + ply')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**.**`)
+  } else 
+     if (message.content === (adminprefix + "leave")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**.**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`LastCodes   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/One");
+      message.channel.send(`**.**`)
+  }
+  if (message.content.startsWith(adminprefix + 'name')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`**.** `)
+} else
+if (message.content.startsWith(adminprefix + 'avatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`**.** `);
+}
+});
+
+
+
+
+
+client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
