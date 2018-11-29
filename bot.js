ace03b7b0189481fcecdd4bbeddfbe0fa80b9d85
Sellmox5 <42848880+SPLITLPTEOEORK2003@users.noreply.github.com>
2018-11-29 19:24:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,42 +2,40 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "S";
 var adminprefix = 'S'
+
 const developers = ["350547695569600522"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
       
-  if (message.content.startsWith(adminprefix + ply')) {
+  if (message.content.startsWith(adminprefix + 'ply')) {
     client.user.setGame(argresult);
-      message.channel.send(`**.**`)
+      message.channel.send(`✅`)
   } else 
      if (message.content === (adminprefix + "leave")) {
     message.guild.leave();        
   } else  
   if (message.content.startsWith(adminprefix + 'wt')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`**.**`)
+      message.channel.send(`✅`)
   } else 
   if (message.content.startsWith(adminprefix + 'ls')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`LastCodes   ${argresult}**`)
+      message.channel.send(`✅`)
   } else 
   if (message.content.startsWith(adminprefix + 'st')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/One");
-      message.channel.send(`**.**`)
+    client.user.setGame(argresult, "https://www.twitch.tv/dream");
+      message.channel.send(`**✅**`)
   }
-  if (message.content.startsWith(adminprefix + 'name')) {
+  if (message.content.startsWith(adminprefix + 'ne')) {
   client.user.setUsername(argresult).then
-      message.channel.send(`**.** `)
+      message.channel.send(`✅`)
 } else
-if (message.content.startsWith(adminprefix + 'avatar')) {
+if (message.content.startsWith(adminprefix + 'av')) {
   client.user.setAvatar(argresult);
-    message.channel.send(`**.** `);
+    message.channel.send(`✅`)
 }
 });
-
-
-
 
 
 client.login(process.env.TOKEN);// لا تغير فيها شيء
