1109a95973c6ad90107bebb9489524c853ad77a1
Sellmox5 <42848880+SPLITLPTEOEORK2003@users.noreply.github.com>
2019-02-27 18:01:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 var prefix = ".";
 var adminprefix = '.'
 
-const developers = ["544843719455539220"]
+const developers = ["508644081438556160"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
